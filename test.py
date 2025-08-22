410e6e04bd32b9880b41ca5d61cde9fca5079d9b
nahyun0920 <24-10514@gsuite.gen.go.kr>
2025-08-22 16:14:45 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="스포츠 추천기", page_icon="🏃", layout="centered")
 
 st.title("🔥 나에게 딱 맞는 스포츠 찾기 🔥")
-st.write("성격유형과 운동 목적을 선택하면 어울리는 스포츠를 추천해드립니다!")
+st.write("성격유형과 체력 수준을 선택하면 어울리는 스포츠를 추천해드립니다!")
 
 # -----------------------
 # 입력 파트
@@ -17,11 +17,6 @@ personality = st.radio(
      "집중 잘하고 혼자 하는 걸 좋아하는 성격"]
 )
 
-purpose = st.selectbox(
-    "👉 운동 목적을 선택하세요:",
-    ["건강", "다이어트", "경쟁", "재미", "사회성"]
-)
-
 fitness = st.slider("👉 본인의 체력 수준은?", 1, 10, 5)
 
 # -----------------------
@@ -30,24 +25,44 @@ fitness = st.slider("👉 본인의 체력 수준은?", 1, 10, 5)
 recommendations = []
 
 if personality == "활발하고 사교적인 성격":
-    recommendations = ["⚽ 축구 - 친구들과 함께 즐기며 협동심 강화!",
-                       "🏀 농구 - 빠른 템포와 팀워크에 잘 어울려요!"]
+    if fitness <= 4:
+        recommendations = ["🏓 탁구 - 가볍게 즐기기 좋아요!", "🚶 가벼운 걷기 - 친구랑 수다 떨며 딱!"]
+    elif fitness <= 7:
+        recommendations = ["🏀 농구 - 빠른 템포와 팀워크에 어울려요!", "🏸 배드민턴 - 가볍게도, 치열하게도 가능!"]
+    else:
+        recommendations = ["⚽ 축구 - 에너지 넘치는 당신에게 완벽!", "🏐 배구 - 활발함과 체력이 딱 맞아요!"]
 
 elif personality == "차분하고 인내심 강한 성격":
-    recommendations = ["🏊 수영 - 꾸준히 하면 성취감 최고!",
-                       "🏃 마라톤 - 집중력과 인내심을 살릴 수 있어요!"]
+    if fitness <= 4:
+        recommendations = ["🧘 요가 - 호흡과 집중에 좋아요", "🚶‍♂️ 산책 - 꾸준함이 무기!"]
+    elif fitness <= 7:
+        recommendations = ["🏊 수영 - 차분하게 체력 기르기 최고!", "🚴 자전거 - 중간 난이도 지구력 운동"]
+    else:
+        recommendations = ["🏃 마라톤 - 인내심과 체력을 동시에!", "⛰️ 등산 - 자연 속에서 꾸준한 도전"]
 
 elif personality == "도전적이고 승부욕 강한 성격":
-    recommendations = ["🥋 태권도 - 강한 정신력과 승부욕에 어울려요!",
-                       "🤼 유도 - 전략과 힘을 동시에 발휘할 수 있어요!"]
+    if fitness <= 4:
+        recommendations = ["🏓 탁구 - 가볍게 시작하면서 승부욕 발휘!", "🥊 라이트 복싱 - 재미 위주로"]
+    elif fitness <= 7:
+        recommendations = ["🥋 태권도 - 승부욕을 발휘하기 좋은 운동!", "🤼 유도 - 기술과 힘을 동시에!"]
+    else:
+        recommendations = ["🏋️ 크로스핏 - 강한 정신력과 체력 발휘!", "🥊 복싱/격투기 - 고강도 승부 운동"]
 
 elif personality == "창의적이고 자유로운 성격":
-    recommendations = ["💃 댄스 - 자유롭게 표현하며 스트레스 해소!",
-                       "🛹 스케이트보드 - 창의적인 동작으로 개성 발휘!"]
+    if fitness <= 4:
+        recommendations = ["🧘 필라테스 - 유연성과 창의성 발휘!", "💃 가벼운 댄스 - 즐겁게 몸 풀기"]
+    elif fitness <= 7:
+        recommendations = ["🛹 스케이트보드 - 자유롭게 기술 도전!", "🤸‍♀️ 리듬체조 - 표현력 살리기"]
+    else:
+        recommendations = ["🎭 댄스 배틀 - 창의력 폭발!", "⛷️ 스노보드 - 고난도 기술에 도전!"]
 
 elif personality == "집중 잘하고 혼자 하는 걸 좋아하는 성격":
-    recommendations = ["🏹 양궁 - 집중력 발휘에 최고!",
-                       "⛳ 골프 - 혼자만의 플레이에 잘 맞아요!"]
+    if fitness <= 4:
+        recommendations = ["🎯 다트 - 집중력과 손 눈 협응!", "♟️ 체스+체력운동 (간단한 조깅 병행)"]
+    elif fitness <= 7:
+        recommendations = ["🏹 양궁 - 집중력 발휘에 최고!", "🏓 탁구(개인연습) - 혼자도 가능!"]
+    else:
+        recommendations = ["⛳ 골프 - 집중과 체력 모두 활용!", "🎾 테니스(싱글) - 개인 플레이로 도전!"]
 
 # -----------------------
 # 결과 출력
@@ -56,5 +71,5 @@ st.subheader("🎉 추천 결과 🎉")
 for rec in recommendations:
     st.write(rec)
 
-st.info(f"당신의 운동 목적은 **{purpose}**, 체력 수준은 **{fitness}/10** 입니다!")
+st.info(f"체력 수준: **{fitness}/10** 에 맞춘 추천입니다!")
 
